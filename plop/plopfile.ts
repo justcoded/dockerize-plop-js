fd18ac52471927ec45a6ab2b889a1ce914dfbf11
apovkh <apovkh@justcoded.com>
2024-10-18 17:43:10 +0300

    fix: :memo: plop console info

--- plop/plopfile.ts
+++ plop/plopfile.ts
@@ -1,29 +1,15 @@
 import { NodePlopAPI } from 'plop';
-import * as fs from 'fs';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default function (plop: NodePlopAPI) {
   plop.setGenerator('Show README', {
     description: 'Show the content of README.md',
     actions: [
       () => {
-        const readmePath = path.join(__dirname, 'README.md');
-
-        if (fs.existsSync(readmePath)) {
-          const content = fs.readFileSync(readmePath, 'utf-8');
-          console.log(content);
-          return 'README.md content displayed in console.';
-        } else {
-          console.log(`
-            No real generators available. \n
-            To use docker image, mount your plopfile sources in /plop directory.`
-          );
-          return 'README.md file not found!';
-        }
+        console.log(`
+          No real generators available. \n
+          To use docker image, mount your plopfile sources in /plop directory.`
+        );
       }
     ]
   });
